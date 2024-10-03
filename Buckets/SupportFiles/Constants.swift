5a966b0b6cd8f39f958007c203b0ef7096f722d4
Romell Bolton <Romell.Bolton@MorganStanley.com>
2024-10-03 17:13:27 -0500

    Version 3.3.8 Build 1

--- Buckets/SupportFiles/Constants.swift
+++ Buckets/SupportFiles/Constants.swift
@@ -217,7 +217,7 @@ let team: [Int: Image] = [
     32: Logos.WesternConference
 ]
 
-let seasonStartYear2023 = "2023"
+let seasonStartYear2023 = "2024"
 let allStarFriday2025 = "2025-02-14"
 let allStarSaturday2025 = "2025-02-15"
 let noGamesString = "No games scheduled."
